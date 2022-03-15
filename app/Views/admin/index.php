04fe49925375d8b4b76a105663faf6988c4df409
fidel <fideljgr@hotmail.com>
2022-03-15 14:04:37 -0400

    Empezando

--- app/Views/admin/index.php
+++ app/Views/admin/index.php
@@ -1,7 +1,46 @@
 <?= $this->include("includes/header"); ?>
 
-<body>
 
-</body>
 
-</html>
+<div class="container-fluid py-4">
+    <div class="card">
+        <div class="card-header">
+            <h3>Empleados<a href="" class="btn bg-gradient-primary float-end">Nuevo</a></h3>
+        </div>
+        <div class="card-body">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">First</th>
+                        <th scope="col">Last</th>
+                        <th scope="col">Handle</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td colspan="2">Larry the Bird</td>
+                        <td>@twitter</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+</main>
+
+
+<?= $this->include("includes/footer"); ?>
